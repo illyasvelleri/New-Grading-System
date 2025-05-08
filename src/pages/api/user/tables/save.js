e437e730c2ed57fd8299cf58e56c82327dbb2cd2
illyasvelleri <cookon123@gmail.com>
2025-05-08 15:22:20 +0530

    fixed bug

--- src/pages/api/user/tables/save.js
+++ src/pages/api/user/tables/save.js
@@ -1,12 +1,3 @@
-// import { saveUserTable } from "@/controllers/userController";
-
-// export default async function handler(req, res) {
-//     if (req.method === "POST") {
-//         return await saveUserTable(req, res);
-//     } else {
-//         res.status(405).json({ error: "Method Not Allowed" });
-//     }
-// }
 import { saveUserTable } from "@/controllers/userController";
 
 export default async function handler(req, res) {
